4b98059051dd522227bdbf4dec6627f44c468dab
Konstantin Savin <149599523+SumaYray@users.noreply.github.com>
2024-05-20 23:32:55 +0300

    1

--- routes/games.js
+++
@@ -1,7 +0,0 @@
-const gamesRouter = require("express").Router(); // Создали роутер
-const { getAllGames, deleteGame, addGameController } = require("../controllers/games")
-
-gamesRouter.get("/games", getAllGames)
-gamesRouter.delete("/games/:id", deleteGame);
-gamesRouter.post("/games", addGameController) 
-module.exports = gamesRouter;
